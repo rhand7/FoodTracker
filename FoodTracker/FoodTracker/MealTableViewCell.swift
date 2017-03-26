24a93d7b103bbb421dc8b7f8fe51e7a0321e98b3
rhand7 <rob@mmoutside.com>
2017-03-26 15:01:51 -0600

    Add files via upload

---
+++ FoodTracker/FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by handje on 3/26/17.
+//  Copyright © 2017 Rob Hand. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+    
+    //MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControl: RatingControl!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
